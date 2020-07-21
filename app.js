3e20bc709e362d59038b3277099017b540a43f8e
Folarin Isaac <folarinisaac36@gmail.com>
2020-07-22 00:47:47 +0100

    Introduction to Javascript and Data Type

---
+++ app.js
@@ -0,0 +1,35 @@
+//using the built-in function and passing an argument
+console.log('Welcome', 'to', '30', 'days', 'of', 'JavaScript');
+
+//declaring variables
+let firstName = 'Folarin'
+let lastName = 'Isaac'
+let country = 'Nigeria'
+let city = 'Ikorodu'
+let age = 22
+let isMarried = false
+
+console.log(firstName, lastName, country, city, age, isMarried)
+
+//declaring variables with numbers
+const gravity = 9.8
+const PI = 3.142
+const boilingPoint = 100
+
+console.log(gravity, PI, boilingPoint, age)
+
+//declaring variables in one line separated by coma
+let name = 'Folarin',
+    job = 'Software Developer.'
+    live = 'Nigeria'
+    console.log(name, job, live);
+
+//using variables to make a short composition about myself
+let residence = 'Lagos, Nigeria.',
+    hobbies = 'Singing, reading and watching scientific movies.'
+    
+console.log("My name is " + firstName, "and I am a " + job, "I live in " + residence, "My hobbies are " + hobbies);
+
+
+
+
